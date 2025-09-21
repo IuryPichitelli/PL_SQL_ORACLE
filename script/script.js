affdc805decacb777f7bbe890323ab7b153d19b5
Iury <157545189+IuryPichitelli@users.noreply.github.com>
2025-09-21 10:54:06 -0300

    commit

--- script/script.js
+++ script/script.js
@@ -1,4 +1,4 @@
-function iframeFullScreen(){
+/*function iframeFullScreen(){
     const iframeContainer = document.getElementById("iframeVideo")[0];
     const iframeVideo = document.getElementsByClassName('iframe')[0];
 
@@ -9,4 +9,25 @@ function iframeFullScreen(){
 const buttonIframeExpansion = document.querySelector(".buttonIframeExpansion");
 if(buttonIframeExpansion){
     buttonIframeExpansion.addEventListener('click', iframeFullScreen);
-}
+}*/
+
+
+const track = document.querySelector('.carousel');
+    const slides = Array.from(track.children);
+    const nextBtn = document.querySelector('.next');
+    const prevBtn = document.querySelector('.prev');
+    let index = 0;
+
+    function updateCarousel() {
+      track.style.transform = `translateX(${-index * 920}px)`;
+    }
+
+    nextBtn.addEventListener('click', () => {
+      index = (index + 1) % slides.length;
+      updateCarousel();
+    });
+
+    prevBtn.addEventListener('click', () => {
+      index = (index - 1 + slides.length) % slides.length;
+      updateCarousel();
+    });
