3bc68ca1278e243f382d9934bd4af576c2fc1f0f
Iury <157545189+IuryPichitelli@users.noreply.github.com>
2025-09-22 21:44:58 -0300

    style update

--- script/script.js
+++ script/script.js
@@ -1,33 +1,13 @@
-/*function iframeFullScreen(){
-    const iframeContainer = document.getElementById("iframeVideo")[0];
-    const iframeVideo = document.getElementsByClassName('iframe')[0];
 
-    iframeContainer.style.margin = "-20px";
-    iframeVideo.style.width = "80%";
-}
+document.addEventListener("DOMContentLoaded", () => {
+  const btns = document.querySelectorAll('.buttonOpenContainer');
 
-const buttonIframeExpansion = document.querySelector(".buttonIframeExpansion");
-if(buttonIframeExpansion){
-    buttonIframeExpansion.addEventListener('click', iframeFullScreen);
-}*/
-
-
-const track = document.querySelector('.carousel');
-    const slides = Array.from(track.children);
-    const nextBtn = document.querySelector('.next');
-    const prevBtn = document.querySelector('.prev');
-    let index = 0;
-
-    function updateCarousel() {
-      track.style.transform = `translateX(${-index * 920}px)`;
-    }
-
-    nextBtn.addEventListener('click', () => {
-      index = (index + 1) % slides.length;
-      updateCarousel();
+  btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+      const container = btn.nextElementSibling;
+      const button = btn;
+      container.classList.toggle("open");
+      button.classList.toggle("activeImage");
     });
-
-    prevBtn.addEventListener('click', () => {
-      index = (index - 1 + slides.length) % slides.length;
-      updateCarousel();
-    });
+  });
+});
